f32319b484421ddd2abfd6b2c81e95a7e31ed314
w41k3r77020-code <anuradha.raghuwanshi2024@nst.rishihood.edu.in>
2025-09-02 09:58:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-async function cat(filePaths, outputFilePath) {
-  // write your code here
-}
+const fs = require('fs');
+const fsp = fs.promises;
 
+async function cat(filePaths, outputFilePath) {
+  const outputLines = [];
+  for (const filePath of filePaths) {
+    try {
+      const stat = await fsp.stat(filePath);
+      if (stat.isFile()) {
+        const content = await fsp.readFile(filePath, 'utf-8');
+        outputLines.push(content);
+      } else if (stat.isDirectory()) {
+        outputLines.push("Is a directory");
+      } else {
+        outputLines.push("File not found");
+      }
+    } catch (err) {
+      outputLines.push("File not found");
+    }
+  }
+  await fsp.writeFile(outputFilePath, outputLines.join('\n'), 'utf-8');
+}
 module.exports = { cat };
+
